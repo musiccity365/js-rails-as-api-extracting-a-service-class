c2b4f0424dce33a504e031cec16877846598e073
musiccity365 <m_sackstein@yahoo.com>
2021-05-02 02:03:56 -0400

    done

--- db/migrate/20190514145517_create_birds.rb
+++ db/migrate/20190514145517_create_birds.rb
@@ -1,4 +1,4 @@
-class CreateBirds < ActiveRecord::Migration
+class CreateBirds < ActiveRecord::Migration[5.2]
   def change
     create_table :birds do |t|
       t.string :name
--- db/migrate/20190514145524_create_sightings.rb
+++ db/migrate/20190514145524_create_sightings.rb
@@ -1,4 +1,4 @@
-class CreateSightings < ActiveRecord::Migration
+class CreateSightings < ActiveRecord::Migration[5.2]
   def change
     create_table :sightings do |t|
       t.references :bird, foreign_key: true
